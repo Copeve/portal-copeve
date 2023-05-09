c7b8d1b86c8783bf6c250203e6bf7b331136b526
André Oliveira <andrev-eng@ufmg.br>
2023-05-09 15:44:24 -0300

    Implementação do Express no Backend.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/example', (req: Request, res: Response) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
