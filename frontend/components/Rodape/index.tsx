810c35c58da045966c73b151d207a8c0aa6b39c2
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-07-03 16:46:45 -0300

    começando rodapé

--- frontend/components/Rodape/index.tsx
+++ frontend/components/Rodape/index.tsx
@@ -3,15 +3,13 @@ export default function Rodape(){
     return (
         <>
         <footer className={styles.footer} id="footer">
-    
-        <div className={styles.footerSign}>
             <div className={styles.container}>
-                <p><strong>Universidade Federal de Minas Gerais</strong></p>
-                <p>Av. Antonio Carlos, 6627 - Pampulha - Belo Horizonte - MG | CEP 31270-901 | +55 (31) 3409-5000 <br/>
-                    © 2023 Universidade Federal de Minas Gerais. Todos os direitos reservados. <br/>
-                    Desenvolvido no Cedecom UFMG </p>
+                <p className={styles.titulo}>Universidade Federal de Minas Gerais</p>
+                <p>Av. Antonio Carlos, 6627 - Pampulha - Belo Horizonte - MG | CEP 31270-901 | +55 (31) 3409-5000 </p>
+                    <p>© 2023 Universidade Federal de Minas Gerais. Todos os direitos reservados.</p>
+                    <p>Desenvolvido no Cedecom UFMG </p>
             </div>
-        </div>
+
     </footer>
         </>
     
