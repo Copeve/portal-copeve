5c26bf11f2a6a90c39885b30ea952fd4bd047482
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-07-03 15:29:30 -0300

    refatorando cabeçalho

--- frontend/components/Cabecalho/index.tsx
+++ frontend/components/Cabecalho/index.tsx
@@ -2,25 +2,34 @@ import Link from 'next/link';
 import styles from './cabecalho.module.css';
 import Image from 'next/image';
 import {BiSearch} from 'react-icons/bi';
+import { FaBars } from 'react-icons/fa';
+import { RxAccessibility } from 'react-icons/rx';
+import { RiContrastFill } from 'react-icons/ri';
+
 
 export default function Cabecalho(){
     
     return(
         <>
         <header className={styles.cabecalho}>
-        <Image className={styles.cabecalhoLogo} src="/images/LogoUfmg.png" alt="imagem logo" width={200} height={150} />
-        <p className={styles.cabecalhoBarra}>|</p>
-        <h1 className={styles.cabecalhoTitulo}>COPEVE</h1>
-        <ul className={styles.cabecalhoLista}>
-            <li><Link className={styles.listaItem} href="/">Concursos</Link></li>
-            <li ><Link className={styles.listaItem} href="/">Notícias</Link></li>
-            <li ><Link className={styles.listaItem} href="/">Contato</Link></li>
-            <li ><Link className={styles.listaItem} href="/">Concursos Anteriores</Link></li>
-            <li ><Link className={styles.listaItem} href="/">A COPEVE</Link></li>
-            <li ><Link className={styles.listaItem} href="/">UFMG</Link></li>
-        
-     
-        </ul>
+            <div className={styles.cabecalhoTitulo}>
+                <Image className={styles.tituloLogo} src="/images/LogoUfmg.png" alt="imagem logo" width={200} height={150} />
+                <p className={styles.tituloBarra}>|</p>
+                <h1 className={styles.tituloEscrito}>Comissão Permanente de Vestibular</h1>
+            </div>
+            <div className={styles.acessibilidade}>
+                <div className={styles.cabecalhoAcessibilidade}>
+                    <p className={styles.acessibilidadeLogo}>
+                        <RxAccessibility />
+                    </p>
+                    <p className={styles.acessibilidadeEscrito}>Acessibilidade</p>
+                </div>
+                <div className={styles.contraste}>
+                    <p className={styles.contrasteLogo}><RiContrastFill/></p>
+                    <p className={styles.constrasteEscrito}>Contraste</p>
+                </div>
+                <p className={styles.mapa}>Mapa do site</p>
+            </div>
             <form action="/" className={styles.pesquisar}>
                 <input className={styles.cabecalhoPesquisar} type="search" name='pesquisa' id='pesquisa' placeholder='Pesquisar'/>
                 <label className={styles.pesquisarLupa} htmlFor="pesquisa"><BiSearch/></label>
