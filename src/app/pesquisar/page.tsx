00244352077e3d237a3b527686b40c0bb11ef778
PedroTrogo <p.trogo@gmail.com>
2023-11-19 15:49:08 -0300

    Ordenação do resultado da página de pesquisa

--- src/app/pesquisar/page.tsx
+++ src/app/pesquisar/page.tsx
@@ -7,24 +7,35 @@ import { Spacer } from '../components/spacer';
 import { RawToMarkdown } from '../components/react-markdown';
 import ScrollToTop from '../components/scroll-to-top';
 import { twJoin } from 'tailwind-merge';
+import { sortByDate } from '../../utils/sort-by';
 
-type SearchResult = {
-	noticias: {
-		id: number;
-		publishedAt: string;
-		transliterations: {
-			titulo: string;
-			noticia: string;
-		};
-	}[];
-	concursos: {
-		id: number;
-		publishedAt: string;
-		transliterations: {
-			nome: string;
-		};
-	}[];
+type SearchDataNews = {
+	id: number;
+	publishedAt: string;
+	transliterations: {
+		titulo: string;
+		noticia: string;
+	};
 };
+
+type SearchDataContest = {
+	id: number;
+	publishedAt: string;
+	transliterations: {
+		nome: string;
+	};
+};
+
+type SearchData = {
+	noticias: SearchDataNews[];
+	concursos: SearchDataContest[];
+};
+
+type SearchResponse = ({
+	type: 'noticias'
+} & SearchDataNews) | ({
+	type: 'concursos'
+} & SearchDataContest)
 
 type Props = {
 	searchParams: { [key: string]: string | string[] | undefined };
@@ -32,17 +43,13 @@ type Props = {
 
 export default async function SearchPage({ searchParams }: Props) {
 	const term = searchParams.termo;
-	let data: SearchResult;
+	let data: SearchResponse[] = [];
 
 	if (typeof term === 'string' && term.trim()) {
 		data = await getDate(term);
-	} else {
-		data = { noticias: [], concursos: [] };
 	}
 
-	const { concursos, noticias } = data;
-
-	const hasNoResults = concursos.length === 0 && noticias.length === 0;
+	const hasNoResults = data.length === 0;
 
 	return (
 		<main className="flex flex-col">
@@ -77,67 +84,22 @@ export default async function SearchPage({ searchParams }: Props) {
 				</div>
 			) : (
 				<ul className="mt-16">
-					{concursos.map(({ id, publishedAt, transliterations }) => {
-						return (
-							<li key={id}>
-								<Link
-									key={String(id)}
-									prefetch={false}
-									href={`/concursos/${id}`}
-									className="group flex w-full flex-col border-b pb-4 pt-2"
-								>
-									<div className="flex items-center justify-between">
-										<ContentDate date={publishedAt} />
-										<ContentTag label={'Concurso'} />
-									</div>
-
-									<ContentTitle
-										title={transliterations.nome}
-									/>
-								</Link>
-							</li>
-						);
-					})}
-					{noticias.map(({ id, publishedAt, transliterations }) => {
-						return (
-							<li key={id}>
-								<Link
-									key={String(id)}
-									prefetch={false}
-									scroll={false}
-									href={`/noticias/${id}`}
-									className="group flex w-full flex-col border-b pb-4 pt-2"
-								>
-									<div className="flex items-center justify-between">
-										<ContentDate date={publishedAt} />
-										<ContentTag label={'Notícia'} />
-									</div>
-
-									<ContentTitle
-										title={transliterations.titulo}
-									/>
-
-									<RawToMarkdown
-										className="text-sm leading-4"
-										text={transliterations.noticia}
-										components={{
-											a: ({
-												children,
-												href,
-												...props
-											}) => (
-												<span
-													className="text-title_blue underline"
-													{...props}
-												>
-													{children}
-												</span>
-											)
-										}}
-									/>
-								</Link>
-							</li>
-						);
+					{data.map((item) => {
+						if(item.type === 'concursos') {
+							return (
+								<ContestCard 
+									key={item.id}
+									{...item}
+								/>
+							);
+						} else {
+							return (
+								<NewsCard 
+									key={item.id}
+									{...item}
+								/>
+							)
+						}
 					})}
 				</ul>
 			)}
@@ -146,6 +108,70 @@ export default async function SearchPage({ searchParams }: Props) {
 			<Spacer />
 		</main>
 	);
+}
+
+const ContestCard = ({ id, publishedAt, transliterations }: SearchDataContest ) => {
+	return (
+		<li >
+		<Link
+			key={String(id)}
+			prefetch={false}
+			href={`/concursos/${id}`}
+			className="group flex w-full flex-col border-b pb-4 pt-2"
+		>
+			<div className="flex items-center justify-between">
+				<ContentDate date={publishedAt} />
+				<ContentTag label={'Concurso'} />
+			</div>
+
+			<ContentTitle
+				title={transliterations.nome}
+			/>
+		</Link>
+	</li>
+	)
+}
+
+const NewsCard = ({ id, publishedAt, transliterations }: SearchDataNews ) => {
+	return (
+		<li key={id}>
+		<Link
+			key={String(id)}
+			prefetch={false}
+			scroll={false}
+			href={`/noticias/${id}`}
+			className="group flex w-full flex-col border-b pb-4 pt-2"
+		>
+			<div className="flex items-center justify-between">
+				<ContentDate date={publishedAt} />
+				<ContentTag label={'Notícia'} />
+			</div>
+
+			<ContentTitle
+				title={transliterations.titulo}
+			/>
+
+			<RawToMarkdown
+				className="text-sm leading-4"
+				text={transliterations.noticia}
+				components={{
+					a: ({
+						children,
+						href,
+						...props
+					}) => (
+						<span
+							className="text-title_blue underline"
+							{...props}
+						>
+							{children}
+						</span>
+					)
+				}}
+			/>
+		</Link>
+	</li>
+	)
 }
 
 function ContentDate({ date }: { date: string }) {
@@ -185,7 +211,7 @@ function ContentTag({ label }: { label: 'Concurso' | 'Notícia' }) {
 }
 
 async function getDate(term: string) {
-	const data = await api<SearchResult | { error: Record<string, unknown> }>({
+	const data = await api<SearchData | { error: Record<string, unknown> }>({
 		url: '/fuzzy-search/search',
 		strapiQueryParams: [`query=${term}`],
 		fetchOptions: {
@@ -195,5 +221,17 @@ async function getDate(term: string) {
 
 	if (data.error) throw new Error(data.error.message);
 
-	return data as SearchResult;
+	const sortedData: SearchResponse[] = [];
+
+	(data as SearchData).noticias.map(item => sortedData.push({
+		...item,
+		type: 'noticias'
+	}));
+
+	(data as SearchData).concursos.map(item => sortedData.push({
+		...item,
+		type: 'concursos'
+	}));
+
+	return (sortedData as SearchResponse[]).sort((a, b) => sortByDate(new Date(a.publishedAt), new Date(b.publishedAt)));
 }
