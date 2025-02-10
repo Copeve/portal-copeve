0fd80b3382df433a72181cd2881b4348f1a35e95
PedroTrogo <p.trogo@gmail.com>
2025-02-10 20:46:00 -0300

    update: atualização no remotePatterns para abranger os subdominios da ufmg

--- next.config.js
+++ next.config.js
@@ -1,35 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	typescript: {
-		// !! WARN !!
-		// Dangerously allow production builds to successfully complete even if
-		// your project has type errors.
-		// !! WARN !!
 		ignoreBuildErrors: true
 	},
 	images: {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: 'ufmg.br'
+				hostname: '**.ufmg.br'
 			},
 			{
 				protocol: 'https',
 				hostname: 'live.staticflickr.com'
-			},
-
-			//dev only
-			{
-				protocol: 'https',
-				hostname: 'healvets.org'
-			},
-			{
-				protocol: 'http',
-				hostname: 'localhost'
-			},
-			{
-				protocol: 'https',
-				hostname: 'copeve-backend-f46d9526ce84.herokuapp.com'
 			}
 		]
 	}
